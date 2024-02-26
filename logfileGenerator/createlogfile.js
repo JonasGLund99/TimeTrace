5dd22b76b4c0fbdf312881b8c367a31ce579d9d2
unknown <Asbjoernjc@gmail.com>
2024-02-26 09:23:47 +0100

    reformatted log

--- logfileGenerator/createlogfile.js
+++ logfileGenerator/createlogfile.js
@@ -4,7 +4,13 @@ const FILEEVENT = "../logfiles/logfileLoginLogout.txt";
 const SIMULATEUSERINTERVAL = 20000;
 const LOGOUTMIN = 5000;
 const LOGOUTMAX = 60000;
-const EVENTS = {login: Event("login", "A"), logout: Event("logout", "B")};
+class Event {
+  constructor(event, alias) {
+    this.event = event;
+    this.alias = alias;
+  }
+}
+const EVENTS = {login: new Event("login", "A"), logout: new Event("logout", "B")};
 
 
 // Function to generate a random user ID
@@ -17,6 +23,12 @@ function randomTimeInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// Function to generate log entries without user id
+function generateLogEntryWithoutUserID(action) {
+  const timestamp = new Date().toISOString();
+  return `${timestamp} - ${action}\n`;
+}
+
 // Function to generate log entries
 function generateLogEntry(userID, action) {
   const timestamp = new Date().toISOString();
@@ -26,14 +38,14 @@ function generateLogEntry(userID, action) {
 // Function to simulate user logins and logouts
 function simulateUserActivity() {
   const userID = generateUserID();
-  const logEntryLoginAlias = generateLogEntry(userID, EVENTS['login'].alias);
+  const logEntryLoginAlias = generateLogEntryWithoutUserID(EVENTS['login'].alias);
   const logEntryLogin = generateLogEntry(userID, EVENTS['login'].event);
   fs.appendFileSync(FILEEVENT, logEntryLogin);
   fs.appendFileSync(FILEEVENTALIAS, logEntryLoginAlias);
   
   // Simulate logout within 30 seconds - 10 minutes
   setTimeout(() => {
-    const logEntryLogoutAlias = generateLogEntry(userID, EVENTS['logout'].alias);
+    const logEntryLogoutAlias = generateLogEntryWithoutUserID(EVENTS['logout'].alias);
     const logEntryLogout = generateLogEntry(userID, EVENTS['logout'].event);
     fs.appendFileSync(FILEEVENT, logEntryLogout);
     fs.appendFileSync(FILEEVENTALIAS, logEntryLogoutAlias);
@@ -48,9 +60,3 @@ fs.writeFileSync(FILEEVENT, generateLogEntry('SYSTEM', 'initialized\n'));
 fs.writeFileSync(FILEEVENTALIAS, generateLogEntry('SYSTEM', 'initialized\n'));
 console.log('Log file created. Monitoring user activity...');
 
-class Event {
-  constructor(event, alias) {
-    this.event = event;
-    this.alias = alias;
-  }
-}
