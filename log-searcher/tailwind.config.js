aba4407dd1f01f92b2502c4c2bd84faad886ccdd
Jonas Geertsen Lund <jgl21@student.aau.dk>
2024-02-29 09:02:32 +0100

    Add tailwind

---
+++ log-searcher/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+				primary: '#5E6DB3',
+				secondary: '#AFBC36',
+				tertiary: '#1D2530',
+				linkcolor: '#5E6DB3'
+      }
+    }
+  },
+  plugins: [],
+}
+
