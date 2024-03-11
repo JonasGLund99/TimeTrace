e5d8cdb9b4ba2f36b2736b760d14cb9119ecac82
unknown <Asbjoernjc@gmail.com>
2024-03-11 11:29:24 +0100

    Added methods to LogFormatter

---
+++ TimeTrace/src/models/LogFormatter.ts
@@ -0,0 +1,12 @@
+export class LogFormatter{
+    formatLog(log: File): File{
+        throw new Error("Not implemented yet");
+    }
+
+    removeUnmappedEvents(log: File): File{
+        throw new Error("Not implemented yet");
+    }
+    
+    
+    
+}
