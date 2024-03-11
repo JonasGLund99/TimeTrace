78eaff1bfda7d4b8ed61c085fd935a8eaa86bc39
unknown <Asbjoernjc@gmail.com>
2024-03-11 15:46:17 +0100

    Added axios and innitial config for a request

--- TimeTrace/src/models/QueryHandler.ts
+++ TimeTrace/src/models/QueryHandler.ts
@@ -1,15 +1,50 @@
 import { MonaaMatch } from './MonaaMatch';
 import { TREBuilder } from './TREBuilder';
+import axios, { AxiosRequestConfig} from 'axios';
+import fs from 'fs';
+import FormData from 'form-data';
+
 export class QueryHandler {
-
-    public search(TRE: string, mappedFile: File): MonaaMatch[] {
-        // @Todo add request to monaaserver.
-
-        // Todo comment this in when buildTRE is implemented.
-        this.TREBuilder.buildTRE(TRE);
-        throw new Error("Not implemented yet");
+    public config: AxiosRequestConfig<any> = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: "http://localhost:5000/monaa/",
     }
-
     public TREBuilder: TREBuilder = new TREBuilder();
     public mappings: Map<string, string> = new Map<string, string>();
+
+    public async search(TRE: string, mappedFile: File): Promise<MonaaMatch[]> {
+        // @Todo add request to monaaserver.
+        const httpClient = axios.create();
+        const requestBody = new FormData();
+        const file = fs.createReadStream('/C:/Users/jonas/Downloads/input.txt').read();
+        requestBody.append('file', file.read());
+        requestBody.append('regex', '(AB)%(0,20)$');
+        this.config.headers = requestBody.getHeaders();
+        this.config.data = requestBody;
+
+
+        // Make the POST request
+        let response: MonaaServerResponse | undefined;
+        try {
+            response = await httpClient.request(this.config);            
+        } catch (error) {
+            console.log(error);
+        }
+        console.log(response);
+        
+        this.TREBuilder.buildTRE(TRE);
+
+        return [];
+    }
+
+}
+
+interface MonaaServerResponse {
+    message: string;
+    monaa_result: {
+        lines: string[];
+    };
+    regex: string;
+    status: string;
 }
