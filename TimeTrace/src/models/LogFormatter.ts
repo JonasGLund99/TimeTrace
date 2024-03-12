b6997b04b6e4fe795529d7ceb8ae1900e99da3e0
AndersToft20 <atoft20@student.aau.dk>
2024-03-12 11:35:44 +0100

    comments and mapevents

--- TimeTrace/src/models/LogFormatter.ts
+++ TimeTrace/src/models/LogFormatter.ts
@@ -3,38 +3,50 @@
 export class LogFormatter {
     file: File;
 
-    constructor() {
-        this.file = new File(["login 1\nlogout 2\nlogin 3\nlogout 4\n"], "original_log.txt");
+    constructor() { //ONLY TO CREATE FAKE IN MEMORY FILE FOR TESTING
+        this.file = new File(["1 login\n2 logout\n3 login\n4 logout\n"], "original_log.txt");
     }
 
-    async getFileLines(original_log: File): Promise<Array<string>> {
-        console.log("getting lines of original log file");
-        return new Promise<Array<string>>((resolve, reject) => {
+    async getFileLines(original_log: File): Promise<string[]> {
+        return new Promise<string[]>((resolve, reject) => { //must return promise because reader is async
             let reader = new FileReader();
             reader.onload = async (event) => {
-                let textLines = (reader.result as string).split("\n");
-                console.log(textLines);
-                resolve(textLines);
+                let textLines = (reader.result as string).split("\n"); //split file on newlines
+                resolve(textLines); //all went well, return textLines
             };
-            reader.onerror = async (ev) => {
-                console.error("Unable to read file", original_log.name, ev);
-                reject(ev);
+            reader.onerror = async (e) => { //on error reject
+                console.error("Unable to read file", original_log.name, e);
+                reject(e);
             };
-            reader.readAsText(original_log);
+            reader.readAsText(original_log); //call reader
         });
     }
     
     async formatLog(original_log: File, mappings: Map<string, string>): Promise<File> {
         console.log("formatLog");
         try {
-            let lines: Array<string> = await this.getFileLines(original_log);
-            console.log("Log has been formatted.");
-            let f: File = new File(["A 1\nB 2\nA 3\nB 4\n"], "mapped.txt")
+            let lines: string[] = await this.getFileLines(original_log); //convert file to array of strings. Has format <time> <event>
+            let mapped_lines: string[] = this.mapEvents(lines, mappings) //map all events and format to <event> <time>
+            console.log({"mapped_lines": mapped_lines})
+            let f: File = new File([mapped_lines.join("\n")], "mapped.txt") //return file object with mapped and formatted events
             return f;
-        } catch (error) {
+        } catch (error) { //readfile might throw error
             console.log("Error formatting the log.", error);
             throw error;
         } 
+    }
+
+    mapEvents(lines: string[], mappings: Map<string, string>): string[] {
+        let mapped_rows: string[] = [];
+        lines.forEach(line => {
+            let line_elements: string[] = line.split(" ")
+            if (line_elements.length === 2) { //only map rows that has two elements (ignore newlines at bottom etc)
+                let event: string = line_elements[1]; //event is second element
+                let timestamp: string = line_elements[0]; // timestamp is first element
+                mapped_rows.push(mappings.get(event)+ " " + timestamp) //format data <mapped_event> <timestamp>
+            }
+        });
+        return mapped_rows;
     }
 
     removeUnmappedEvents(log: any): any {
