dc9c2206517ebbbd034734c5b3b7a85e5b549d2d
unknown <Asbjoernjc@gmail.com>
2024-03-12 10:09:20 +0100

    Commented out code for testing purposes

--- TimeTrace/src/App.tsx
+++ TimeTrace/src/App.tsx
@@ -4,8 +4,9 @@ import './App.css';
 import { QueryHandler } from './models/QueryHandler';
 
 function App() {
-  const queryHandler = new QueryHandler();
-  queryHandler.search("(AB)%(0,20)$", new File([`A 1\nB 2\nA 3\nA 4\nB 5\nA 6\nB 7\nA 8`], "logMappedAB.txt"));
+  // This is only for testing purposes
+  // const queryHandler = new QueryHandler();
+  // queryHandler.search("(AB)%(0,20)$", new File([`A 1\nB 2\nA 3\nA 4\nB 5\nA 6\nB 7\nA 8`], "logMappedAB.txt"));
 
   return (
     <div className="App">
