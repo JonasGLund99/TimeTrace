881c98da8239d47e2dfd7155c6fc7865a45ad32e
unknown <Asbjoernjc@gmail.com>
2024-03-13 16:20:59 +0100

    The rows are now created depending on the events passed to the LogTable

--- TimeTrace/src/components/LogTable.tsx
+++ TimeTrace/src/components/LogTable.tsx
@@ -1,43 +1,46 @@
 import { useEffect, useState } from "react";
 
 function LogTable(props: LogTableProps) {
-    const [mapping, setMapping] = useState<string>("");
+  const [mapping, setMapping] = useState<string>("");
 
-    const handleMappingChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const inputValue = event.target.value;
-        // Filter out characters that are not in the range of 'a' to 'z'
-        const filteredValue = inputValue.replace(/[^a-yA-Y]]/gi, '');
-        if(filteredValue === "") return;
-        
-        setMapping(filteredValue);
-    };
+  const handleMappingChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+    // Filter out characters that are not in the range of 'a' to 'z'
+    const filteredValue = inputValue.replace(/[^a-yA-Y]]/gi, "");
+    if (filteredValue === "") return;
 
-return (
-    <div className="flex">
-        <span>
-            2024-02-26T08:22:14.642Z some_event.xxxxxxxxsdsdsdsdsds
-        </span>
+    setMapping(filteredValue);
+  };
 
-        {props.displaysMappings && (
-            <div>
-                
-                <input type="text" value={mapping} onChange={(event) => {
+  return (
+    <>
+      {props.events.map((event: string) => {
+        return (
+            <div className="flex">
+            <span>{event}</span>
+    
+            {props.displaysMappings && (
+              <div>
+                <input
+                  type="text"
+                  value={mapping}
+                  onChange={(event) => {
                     handleMappingChange(event);
-                }} />
-                <button>
-                    Remove this mapping
-                </button>
-            </div>
+                  }}
+                />
+                <button>Remove this mapping</button>
+              </div>
             )}
-        
-    </div>
-);
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export type LogTableProps = {
-    displaysMappings: boolean | undefined;
-    events: string[];
-}
-  
+  displaysMappings: boolean | undefined;
+  events: string[];
+};
 
 export default LogTable;
--- TimeTrace/src/pages/MappingsPage.tsx
+++ TimeTrace/src/pages/MappingsPage.tsx
@@ -6,7 +6,7 @@ function MappingsPage() {
     return (
       <div>
 		    <h1>MappingsPage</h1>
-        <LogTable displaysMappings={true} events={["event1", "event2", "event3"]}/>
+        <LogTable displaysMappings={true} events={["2024-02-26T08:22:14.642Z some_event.xxxxxxxxsdsdsdsdsds", "2024-02-26T08:22:14.648Z some_event.xxxxxxxxsdsdsdsdsds", "2024-02-26T08:22:14.650Z some_event.xxxxxxxxsdsdsdsdsds"]}/>
       </div>
         
     );
