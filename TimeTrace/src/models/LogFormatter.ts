68510947d0243dcd709f9c029640d1b1400fd21d
AndersToft20 <162872116+aatRanderstegl@users.noreply.github.com>
2024-03-15 08:47:43 +0100

    from python to mf ts

--- TimeTrace/src/models/LogFormatter.ts
+++ TimeTrace/src/models/LogFormatter.ts
@@ -1,4 +1,4 @@
-
+import { getFileLines } from "./helpers/getFileLines";
 
 export class LogFormatter {
 
@@ -8,7 +8,7 @@ export class LogFormatter {
     // }
     
 
-    async getFileLines(original_log: File): Promise<string[]> {
+    async getFileLines(originalLog: File): Promise<string[]> {
         return new Promise<string[]>((resolve, reject) => { //must return promise because reader is async
             let reader = new FileReader();
             reader.onload = async (event) => {
@@ -16,19 +16,19 @@ export class LogFormatter {
                 resolve(textLines); //all went well, return textLines
             };
             reader.onerror = async (e) => { //on error reject
-                console.error("Unable to read file", original_log.name, e);
+                console.error("Unable to read file", originalLog.name, e);
                 reject(e);
             };
-            reader.readAsText(original_log); //call reader
+            reader.readAsText(originalLog); //call reader
         });
     }
     
-    async formatLog(original_log: File, mappings: Map<string, string>): Promise<File> {
+    async formatLog(originalLog: File, mappings: Map<string, string>): Promise<File> {
         try {
-            let lines: string[] = await this.getFileLines(original_log); //convert file to array of strings. Has format <time> <event>
-            let mapped_lines: string[] = this.convertLines(lines, mappings) //map all events and format to <event> <time>
-            console.log({"mapped_lines": mapped_lines})
-            let f: File = new File([mapped_lines.join("\n")], "mapped.txt") //return file object with mapped and formatted events
+            let lines: string[] = await getFileLines(originalLog); //convert file to array of strings. Has format <time> <event>
+            let mappedLines: string[] = this.convertLines(lines, mappings) //map all events and format to <event> <time>
+            console.log({"mappedLines": mappedLines})
+            let f: File = new File([mappedLines.join("\n")], "mapped.txt") //return file object with mapped and formatted events
             return f;
         } catch (error) { //readfile might throw error
             console.log("Error formatting the log.", error);
@@ -37,26 +37,26 @@ export class LogFormatter {
     }
 
     convertLines(lines: string[], mappings: Map<string, string>): string[] {
-        let mapped_rows: string[] = [];
-        let map_value: string;
+        let mappedRows: string[] = [];
+        let mapValue: string;
         lines.forEach(line => {
-            let line_elements: string[] = line.split(" ")
-            if (line_elements.length === 2) { //only map rows that has two elements (ignore newlines at bottom etc)
-                let event: string = line_elements[1]; //event is second element
-                let timestamp: string = line_elements[0]; // timestamp is first element
-                map_value = this.getMapValue(event, mappings)
-                mapped_rows.push(map_value+ " " + this.convertDateformat(timestamp)) //format data <mapped_event> <timestamp>
+            let lineElements: string[] = line.split(" ")
+            if (lineElements.length === 2) { //only map rows that has two elements (ignore newlines at bottom etc)
+                let event: string = lineElements[1]; //event is second element
+                let timestamp: string = lineElements[0]; // timestamp is first element
+                mapValue = this.getMapValue(event, mappings)
+                mappedRows.push(mapValue+ " " + this.convertDateformat(timestamp)) //format data <mapped_event> <timestamp>
             }
         });
-        return mapped_rows;
+        return mappedRows;
     }
 
     getMapValue(event: string, mappings: Map<string, string>): string {
-        let map_value: string | undefined = mappings.get(event) //try to map value
-        if (map_value === undefined) { //if value was not mapped then map to Z
-            map_value = "Z"
+        let mapValue: string | undefined = mappings.get(event) //try to map value
+        if (mapValue === undefined) { //if value was not mapped then map to Z
+            mapValue = "Z"
         }
-        return map_value
+        return mapValue
     }
 
     convertDateformat(timestamp: string): string {
