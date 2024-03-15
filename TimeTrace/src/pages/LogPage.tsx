cfba0637d62722c1160de11ce353488e24f0f510
caspermortensen <cmor21@student.aau.dk>
2024-03-15 12:12:18 +0100

    connect TRE input to search function in query handler + some tailwind styling in the input field

--- TimeTrace/src/pages/LogPage.tsx
+++ TimeTrace/src/pages/LogPage.tsx
@@ -7,9 +7,8 @@ function LogPage() {
 
     console.log('Search TRE:', TRE);
     queryHandler.TREBuilder.TREString = TRE;
-
+    queryHandler.search(TRE);
   }
-
   return (
     <div>
       <h1 className="flex justify-center pb-5 text-4xl ">Search logfile</h1>
@@ -17,7 +16,6 @@ function LogPage() {
     </div>
 
   );
-
 }
 
 
