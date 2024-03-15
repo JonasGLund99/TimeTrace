865faa615848a688388a8e766447d58193ea51a8
unknown <Asbjoernjc@gmail.com>
2024-03-15 11:19:27 +0100

    removed console.log

--- TimeTrace/src/components/LogTable.tsx
+++ TimeTrace/src/components/LogTable.tsx
@@ -4,21 +4,21 @@ function LogTable(props: LogTableProps) {
   const [mappings, setMapping] = useState<Map<string, string>>(new Map(props.events.map(event => [event, ""])));
 
   function handleMappingChange(eventText: string, mappingIndex: number): void {
-    console.log([...props.events.map((val) => "")]);
     const inputValue = eventText;
     
     // Filter out characters that are not in the range of 'a' to 'y' || 'A' to 'Y'
     const lastChar = inputValue.slice(-1);
     const filteredValue = lastChar.search(/[a-yA-Y]/gi) === 0 ? lastChar : "";
 
-    //eventText is empty when the user has removed the mapping.
+    // EventText is empty when the user has removed the mapping.
     if (filteredValue === "" && eventText !== "") return;
     const mapKey = props.events[mappingIndex];
     mappings.set(mapKey, filteredValue);
     setMapping(new Map(mappings));
   };
 
-  function removeMapping(event: React.MouseEvent<SVGSVGElement>): void {
+  function removeMapping(eventIndex: number): void {
+    handleMappingChange("", eventIndex);
   }
 
 
@@ -39,7 +39,7 @@ function LogTable(props: LogTableProps) {
                     handleMappingChange(event.target.value, i);
                   }}
                 />
-                <svg onClick={(event) => {removeMapping(event)}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 cursor-pointer">
+                <svg onClick={() => {removeMapping(i)}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 cursor-pointer">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                 </svg>
 
