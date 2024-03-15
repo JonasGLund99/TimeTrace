f0569ff228139f86c5604dfb40ee0dbf164c257f
caspermortensen <cmor21@student.aau.dk>
2024-03-15 13:05:33 +0100

    color change on button

--- TimeTrace/src/components/SearchForm.tsx
+++ TimeTrace/src/components/SearchForm.tsx
@@ -29,7 +29,7 @@ export default function Search({ onSubmit }: SearchProps) {
           required
         />
         <button
-          className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="submit">Search</button>
+          className="text-white absolute end-2.5 bottom-2.5 bg-gray-800 hover:bg-gray-700  font-medium rounded-lg text-sm px-4 py-2 " type="submit">Search</button>
       </div>
     </form>
   );
