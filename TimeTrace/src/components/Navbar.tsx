a1efd3f86a6d749c7e12209cf4412c741c27c5e3
Jonas Geertsen Lund <jgl21@student.aau.dk>
2024-03-21 09:12:18 +0100

    Indentation

--- TimeTrace/src/components/Navbar.tsx
+++ TimeTrace/src/components/Navbar.tsx
@@ -1,37 +1,37 @@
 import { Link, useLocation } from "react-router-dom";
 
 const navigation = [
-  { name: "Home", href: "/" },
-  { name: "Create mappings", href: "/create-mappings" },
-  { name: "View log", href: "/view-log" },
+    { name: "Home", href: "/" },
+    { name: "Create mappings", href: "/create-mappings" },
+    { name: "View log", href: "/view-log" },
 ];
 
 function classNames(...classes: String[]) {
-  return classes.filter(Boolean).join(" ");
+    return classes.filter(Boolean).join(" ");
 }
 
 function Navbar() {
-  const { pathname } = useLocation();
+    const { pathname } = useLocation();
 
-  return (
-    <nav className="w-full h-[6%] p-2 bg-gray-800 flex items-center space-x-4">
-      {navigation.map((item) => (
-        <Link
-          key={item.name}
-          to={item.href}
-          className={classNames(
-            pathname === item.href
-              ? "bg-gray-900 text-white"
-              : "text-gray-300 hover:bg-gray-700 hover:text-white",
-            "rounded-md px-6 py-2 text-sm font-medium"
-          )}
-          aria-current={pathname === item.href ? "page" : undefined}
-        >
-          {item.name}
-        </Link>
-      ))}
-    </nav>
-  );
+    return (
+        <nav className="w-full h-[6%] p-2 bg-gray-800 flex items-center space-x-4">
+            {navigation.map((item) => (
+                <Link
+                    key={item.name}
+                    to={item.href}
+                    className={classNames(
+                        pathname === item.href
+                            ? "bg-gray-900 text-white"
+                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                        "rounded-md px-6 py-2 text-sm font-medium"
+                    )}
+                    aria-current={pathname === item.href ? "page" : undefined}
+                >
+                    {item.name}
+                </Link>
+            ))}
+        </nav>
+    );
 }
 
 export default Navbar;
--- TimeTrace/src/components/SearchForm.tsx
+++ TimeTrace/src/components/SearchForm.tsx
@@ -1,38 +1,38 @@
 import { useState } from 'react';
 
 interface SearchProps {
-  onSubmit: (query: string) => void;
+    onSubmit: (query: string) => void;
 }
 
 export default function Search({ onSubmit }: SearchProps) {
-  const [TRE, setQuery] = useState('');
+    const [TRE, setQuery] = useState('');
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    onSubmit(TRE);
-  };
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        onSubmit(TRE);
+    };
 
-  return (
-    <form className="max-w-md mx-auto" onSubmit={handleSubmit}>
-      <div className="relative">
-        <div className="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
-          <svg className="w-4 h-4 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-          </svg>
-        </div>
-        <input
-          className="block w-full p-4 text-sm text-gray-900 bg-white border border-gray-600 rounded-lg ps-10 focus:ring-blue-500 focus:border-blue-500"
-          placeholder='Enter TRE string'
-          name="TRE"
-          value={TRE}
-          onChange={(e) => setQuery(e.target.value)}
-          required
-        />
-        <button
-          className="text-white absolute end-2.5 bottom-2.5 bg-gray-800 hover:bg-gray-700  font-medium rounded-lg text-sm px-4 py-2 " type="submit">Search</button>
-      </div>
-    </form>
-  );
+    return (
+        <form className="max-w-md mx-auto" onSubmit={handleSubmit}>
+            <div className="relative">
+                <div className="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
+                    <svg className="w-4 h-4 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                    </svg>
+                </div>
+                <input
+                    className="block w-full p-4 text-sm text-gray-900 bg-white border border-gray-600 rounded-lg ps-10 focus:ring-blue-500 focus:border-blue-500"
+                    placeholder='Enter TRE string'
+                    name="TRE"
+                    value={TRE}
+                    onChange={(e) => setQuery(e.target.value)}
+                    required
+                />
+                <button
+                    className="text-white absolute end-2.5 bottom-2.5 bg-gray-800 hover:bg-gray-700  font-medium rounded-lg text-sm px-4 py-2 " type="submit">Search</button>
+            </div>
+        </form>
+    );
 }
 
 
