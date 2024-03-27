853e9a08e0c26cef93beeab45b14eb3a8c10e5a8
AndersToft20 <93765094+AndersToft20@users.noreply.github.com>
2024-03-27 14:02:09 +0100

    validate that timestamp is the first part of all file lines

--- TimeTrace/src/models/helpers/extractEventsFromFileLines.ts
+++ TimeTrace/src/models/helpers/extractEventsFromFileLines.ts
@@ -1,8 +1,11 @@
 import { extractTimeStamp } from "./extractTimeStamp";
 export function extractEventsFromFileLines(fileLines: string[]): string[] {
 
-    let newLines = fileLines.map((line) => {
+    let newLines = fileLines.map((line, i) => {
         let timestamp: string = extractTimeStamp(line);
+        if (!line.startsWith(timestamp)) {
+            throw new Error("Fileline in file was not formatted correctly. <br /> <br />Time of event must be first part of each line in the file. <br /> <br/>Fileline with error: "+(i+1) + " " + line)
+        }
         line = line.replace(timestamp, "")
         line = line.replace(/(^\s*)|(\s*$)/gi,""); //remove leading and trailing spaces
         line = line.replace(/\n /,"\n"); //remove newlines
