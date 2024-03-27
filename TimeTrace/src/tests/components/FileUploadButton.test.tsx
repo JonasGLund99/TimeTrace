e8572e76dd5ea76bf1d7d66931ca6944aa28f6ed
caspermortensen <cmor21@student.aau.dk>
2024-03-27 15:50:39 +0100

    add test for uploadFileButtonComponent. Check filechange when a file is uploaded or removed.  added testId to handlefileremove button

---
+++ TimeTrace/src/tests/components/FileUploadButton.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, fireEvent, getByTestId } from '@testing-library/react';
+import FileUploadButton from '../../components/FileUploadButton';
+
+describe('FileUploadButton component', () => {
+    describe('fileUpload', () => {
+        test('onFileChange has a file when a file is uploaded', () => {
+            const onFileChangeMock = jest.fn();
+            const { getByLabelText } = render(<FileUploadButton onFileChange={onFileChangeMock} />);
+            const fileInput = getByLabelText('Upload file');
+
+            const file = new File(['file content'], 'example.txt', { type: 'text/plain' });
+            fireEvent.change(fileInput, { target: { files: [file] } });
+
+            expect(onFileChangeMock).toHaveBeenCalledWith(file);
+        });
+    });
+
+    describe('fileRemove', () => {
+        test('onFileChange set to null when file is removed', () => {
+            const onFileChangeMock = jest.fn();
+            const { getByLabelText } = render(<FileUploadButton onFileChange={onFileChangeMock} />);
+            const fileInput = getByLabelText('Upload file');
+
+            const file = new File(['file content'], 'example.txt', { type: 'text/plain' });
+            fireEvent.change(fileInput, { target: { files: [file] } });
+
+            const removeButton = getByTestId(document.body, 'remove-button');
+            fireEvent.click(removeButton);
+
+            expect(onFileChangeMock).toHaveBeenCalledWith(null);
+        });
+    });
+});
+
