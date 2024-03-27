726259c320a84111297851b5dcf90f799a36ed8a
caspermortensen <cmor21@student.aau.dk>
2024-03-27 13:39:57 +0100

    added test for formatLog function in LogFormatter

--- TimeTrace/src/tests/models/LogFormatter.test.ts
+++ TimeTrace/src/tests/models/LogFormatter.test.ts
@@ -1,12 +1,26 @@
 import { LogFormatter } from '../../models/LogFormatter';
+import * as fs from 'fs';
 
 
 describe('LogFormatter', () => {
     describe('formatLog', () => {
-        test('Should return file object with mapped and formatted events', () => {
+        test('Should return file object with mapped and formatted events', async () => {
             // Arrange
+            const logFormatter = new LogFormatter();
+            const filePath = './src/tests/exampleLogFile.txt'
+            const fileContents: string = fs.readFileSync(filePath, 'utf-8');
+            const logfile = new File([fileContents], 'file.txt', { type: 'text/plain' });
+            console.log(logfile);
+            const mappings: Map<string, string> = new Map<string, string>();
+            mappings.set('login', 'A').set('login', 'A').set('delete', 'Z').set('login', 'A').set('login', 'A').set('logout', 'B'); 
+            const expfilePath = './src/tests/exampleMonaaFormatLogFile.txt'
+            const expfileContents: string = fs.readFileSync(expfilePath, 'utf-8');
+            const expectedlogfile = new File([expfileContents], 'file.txt', { type: 'text/plain' });
+    
             // Act
+            const acutalFormattedLogFile =  await logFormatter.formatLog(logfile, mappings)
             // Assert
+            expect(acutalFormattedLogFile).toEqual(expectedlogfile);
             
         });
     });
@@ -80,27 +94,27 @@ describe('LogFormatter', () => {
         });
     });
     describe('convertDateformat', () => {
-        test('Should take a timstamp as string and convert it into EPOK time in string ', () => {
+        test('Should take a timstamp as string and convert it into EPOCH time in string ', () => {
             // Arrange
             const logFormatter = new LogFormatter();
             const timestamp: string = "2024-02-26T08:22:36.677645Z";
 
-            const expectedEPOKTime: string = "1708935756677";
+            const expectedEPOCHTime: string = "1708935756677";
             // Act
             const timestampInMiliseconds = logFormatter.convertDateformat(timestamp);
             // Assert
-            expect(timestampInMiliseconds).toEqual(expectedEPOKTime);
+            expect(timestampInMiliseconds).toEqual(expectedEPOCHTime);
         });
-        test('Expect not equal, assert a wrong expected EPOK time', () => {
+        test('Expect not equal, assert a wrong expected EPOCH time', () => {
             // Arrange
             const logFormatter = new LogFormatter();
             const timestamp: string = "2024-02-26T08:22:36.677645Z";
 
-            const expectedEPOKTime: string = "1708935756678";
+            const expectedEPOCHTime: string = "1708935756678";
             // Act
             const timestampInMiliseconds = logFormatter.convertDateformat(timestamp);
             // Assert
-            expect(timestampInMiliseconds).not.toEqual(expectedEPOKTime);
+            expect(timestampInMiliseconds).not.toEqual(expectedEPOCHTime);
         });
     }); 
 });
