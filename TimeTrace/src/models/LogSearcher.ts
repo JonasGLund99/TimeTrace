19ad2f628d11622b7188c6b1a69dc5d1ba9c06f8
Bone <Asbjoernjc@gmail.com>
2024-03-30 13:24:06 +0100

    Got times for 'optimized' func and non-optimized

--- TimeTrace/src/models/LogSearcher.ts
+++ TimeTrace/src/models/LogSearcher.ts
@@ -1,72 +1,50 @@
-import { MonaaZone } from './MonaaZone';
-import { SearchInterval } from './SearchInterval';
+import { MonaaZone } from "./MonaaZone";
+import { SearchInterval } from "./SearchInterval";
 import { extractTimeStamp } from "./helpers/extractTimeStamp";
 
 export class LogSearcher {
+  findZones(logFile: string[], SearchIntervals: SearchInterval[]): MonaaZone[] {
+    console.time("findZones");
+    const MonaaZoneMatches: MonaaZone[] = [];
+    const timestamps: number[] = [];
+    let averageTimegrowth: number = 0;
+    let prevLineTime: number = 0;
 
-    findZones(logFile: string[], SearchIntervals: SearchInterval[]): MonaaZone[] {
-        console.time("findZones");
-        const MonaaZoneMatches: MonaaZone[] = [];
-        // const timestamps: number[] = [];
-        // let averageTimegrowth: number = 0;
-        // let prevLineTime: number = 0;
+    logFile.forEach((line: string, lineIndex: number) => {
+      const timestampISO: string = extractTimeStamp(line);
+      const eventTimeStamp = new Date(timestampISO).getTime();
+      timestamps.push(eventTimeStamp);
+      averageTimegrowth =
+        (averageTimegrowth + (eventTimeStamp - prevLineTime)) / 2;
+      prevLineTime = eventTimeStamp;
+    });
 
-        // logFile.forEach((line: string, lineIndex: number) => {
-        //     const timestampISO: string = extractTimeStamp(line);
-        //     const eventTimeStamp = new Date(timestampISO).getTime();
-        //     timestamps.push(eventTimeStamp);
-        //     averageTimegrowth = (averageTimegrowth + (eventTimeStamp - prevLineTime))/2;
-        //     prevLineTime = eventTimeStamp;
-        // });
+    for (let i = 0; i < SearchIntervals.length; i++) {
+      let foundmatch = new MonaaZone();
+      const firstTimestamp = timestamps[0];
+      const difference = SearchIntervals[i].start - firstTimestamp;
+      const multiplum = difference / averageTimegrowth;
+      let startingIndex = Math.floor(multiplum);
+      while (timestamps[startingIndex] > SearchIntervals[i].start) {
+        startingIndex--;
+      }
 
-        for (let i = 0; i < SearchIntervals.length; i++) {
-            let foundmatch = new MonaaZone();
-        //     const firstTimestamp = timestamps[0]; 
-        //     const difference = SearchIntervals[i].start - firstTimestamp;
-        //     const multiplum = (SearchIntervals[i].start - firstTimestamp) / averageTimegrowth;
-        //     let startingIndex = Math.floor(multiplum); 
-        //     while (timestamps[startingIndex] > SearchIntervals[i].start) {
-        //         console.log("startingIndex was past the start of the interval, moving back one step.")
-        //         startingIndex--;
-        //     }
+      for (let j = startingIndex; j < timestamps.length; j++) {
+        const element = timestamps[j];
 
-        //     for (let j = startingIndex; j < timestamps.length; j++) {
-        //         const element = timestamps[j];
-
-        //         if (element >= SearchIntervals[i].start && element <= SearchIntervals[i].end) {
-        //             foundmatch.lineMatches.push(j);
-        //         }
-        //         else if (element > SearchIntervals[i].end) {
-        //             break;
-        //         }
-        //     }
-
-            for (let j = 0; j < logFile.length; j++) {
-                const element = logFile[j];
-                let timestampISO: string = extractTimeStamp(element);
-                const eventTimeStamp = new Date(timestampISO).getTime();
-                if (eventTimeStamp >= SearchIntervals[i].start && eventTimeStamp <= SearchIntervals[i].end) {
-                    foundmatch.lineMatches.push(j)
-                }
-                else if (eventTimeStamp > SearchIntervals[i].end) {
-                    break;
-                }
-                
-            }
-            // logFile.forEach((line: string, lineIndex: number) => {
-            //     let timestampISO: string = extractTimeStamp(line);
-            //     const eventTimeStamp = new Date(timestampISO).getTime();
-            //     if (eventTimeStamp >= SearchIntervals[i].start && eventTimeStamp <= SearchIntervals[i].end) {
-            //         foundmatch.lineMatches.push(lineIndex)
-            //     }
-            //     else if (eventTimeStamp > SearchIntervals[i].end) {
-            //         break;
-            //     }
-            // });
-            MonaaZoneMatches.push(foundmatch);
+        if (
+          element >= SearchIntervals[i].start &&
+          element <= SearchIntervals[i].end
+        ) {
+          foundmatch.lineMatches.push(j);
+        } else if (element > SearchIntervals[i].end) {
+          break;
         }
-        console.timeEnd("findZones");
-        console.log("MonaaZoneMatches", MonaaZoneMatches)
-        return MonaaZoneMatches
-    };
+      }
+      MonaaZoneMatches.push(foundmatch);
+    }
+    console.timeEnd("findZones");
+    console.log("MonaaZoneMatches", MonaaZoneMatches);
+    return MonaaZoneMatches;
+  }
 }
