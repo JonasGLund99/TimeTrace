2df921e6a3127788b3b5a74b2d3c96456729e4bc
caspermortensen <cmor21@student.aau.dk>
2024-04-01 15:18:06 +0200

    input field (outcommented)

--- TimeTrace/src/components/LogTable.tsx
+++ TimeTrace/src/components/LogTable.tsx
@@ -236,7 +236,19 @@ function LogTable({ mappingsAreEditable }: LogTableProps) {
                         onClick={() => { setMonaaMatchIndex(monaaMatchIndex === 0 ? 0 : monaaMatchIndex - 1) }}>
                         Previous match
                     </button>
-                    <div className="text-gray-800 ">
+                    <div id="monaa-match-input" className="text-gray-800 ">
+                        {/* TODO:
+                        <input
+                            className="text-right "
+                            type="number"
+                            name="matchIndex"
+                            id=""
+                            value={monaaMatchIndex + 1}
+                            min={1}
+                            max={matches.length}
+                            onChange={(e) => {
+                                setMonaaMatchIndex((e.target.value as unknown as number) - 1);
+                            }} /> */}
                         {monaaMatchIndex + 1} / {matches.length}
                     </div>
                     <button className="px-4 py-2 text-sm font-medium text-white bg-gray-800 rounded-lg hover:bg-gray-700 "
