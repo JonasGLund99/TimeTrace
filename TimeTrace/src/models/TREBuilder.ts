e4fec264d1f23ffd2825d8c039b01b8c3145c2a2
unknown <Asbjoernjc@gmail.com>
2024-04-02 11:41:11 +0200

    Created static classes

--- TimeTrace/src/models/TREBuilder.ts
+++ TimeTrace/src/models/TREBuilder.ts
@@ -1,10 +1,10 @@
 import { TREParam } from './TREParam';
-export class TREBuilder {
-    TREParams: TREParam[] = [];
-    TREString: string = ""
+export abstract class TREBuilder {
+    public static TREParams: TREParam[] = [];
+    public static TREString: string = ""
 
 
-    buildTRE(TREString: string): string {
+    public static buildTRE(TREString: string): string {
         this.TREString = TREString;
 
         for (const TreParam of this.TREParams) {
