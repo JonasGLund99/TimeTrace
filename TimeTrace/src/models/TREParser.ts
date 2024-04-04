97ce4d3f1a0ff033751ac891cef3c588261f914f
Bone <Asbjoernjc@gmail.com>
2024-04-04 08:46:14 +0200

    Created class skeleton for TREParser.ts

--- TimeTrace/src/models/TREParser.ts
+++ TimeTrace/src/models/TREParser.ts
@@ -1,3 +1,25 @@
-export abstract class TREParser{
-    
+export abstract class TREParser {
+  public static parseTRE(tre: string): string {
+    return tre;
+  }
+
+  public static isValidGroup(): boolean {
+    return true;
+  }
+
+  public static isValidTimeConstraint(): boolean {
+    return true;
+  }
+
+  public static isValidTimeUnit(): boolean {
+    return true;
+  }
+
+  public static isValidSymbol(): boolean {
+    return true;
+  }
+
+  public static isSymbolMapped(): boolean {
+    return true;
+  }
 }
