1b419e2fa0b0deeae435430a5e7b31a0b5ffef34
caspermortensen <cmor21@student.aau.dk>
2024-04-10 08:23:16 +0200

    home setup

--- TimeTrace/src/pages/Home.tsx
+++ TimeTrace/src/pages/Home.tsx
@@ -15,7 +15,93 @@ function Home() {
     }, [])
 
     return (
-        <h1 className="">Home</h1>
+        <div className="flex flex-row justify-between h-full">
+            <div className="w-[15%] h-full border border-gray-400">
+                <h1 className="">Home</h1>
+            </div>
+            <div className="w-[83%] h-full border border-gray-400 rounded-lg flex justify-center">
+
+                <div className="w-full m-6" id="accordion-nested-parent" data-accordion="collapse">
+                    <h2 id="accordion-collapse-heading-1">
+                        <button type="button" className="flex items-center justify-between w-full gap-3 p-5 font-medium text-gray-500 border border-b-0 border-gray-200 rtl:text-right rounded-t-xl focus:ring-4 focus:ring-gray-200 hover:bg-gray-100 " data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+                            <span>What is Flowbite?</span>
+                            <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5" />
+                            </svg>
+                        </button>
+                    </h2>
+                    <div id="accordion-collapse-body-1" className="hidden" aria-labelledby="accordion-collapse-heading-1">
+                        <div className="p-5 border border-b-0 border-gray-200 ">
+                            <p className="mb-2 text-gray-500 ">Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.</p>
+                            <p className="mb-2 text-gray-500 ">Check out this guide to learn how to <a href="/docs/getting-started/introduction/" className="text-blue-600 hover:underline">get started</a> and start developing websites even faster with components on top of Tailwind CSS.</p>
+                            <p className="mb-4 text-gray-500 ">What are the differences between Flowbite and Tailwind UI?</p>
+                            {/* <!-- Nested accordion --> */}
+                            <div id="accordion-nested-collapse" data-accordion="collapse">
+                                <h2 id="accordion-nested-collapse-heading-1">
+                                    <button type="button" className="flex items-center justify-between w-full gap-3 p-5 font-medium text-gray-500 border border-b-0 border-gray-200 rounded-t-xl rtl:text-right focus:ring-4 focus:ring-gray-200 hover:bg-gray-100" data-accordion-target="#accordion-nested-collapse-body-1" aria-expanded="false" aria-controls="accordion-nested-collapse-body-1">
+                                        <span>Open source</span>
+                                        <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5" />
+                                        </svg>
+                                    </button>
+                                </h2>
+                                <div id="accordion-nested-collapse-body-1" className="hidden" aria-labelledby="accordion-nested-collapse-heading-1">
+                                    <div className="p-5 border border-b-0 border-gray-200 ">
+                                        <p className="text-gray-500 ">The main difference is that the core components from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product.</p>
+                                    </div>
+                                </div>
+                                <h2 id="accordion-nested-collapse-heading-2">
+                                    <button type="button" className="flex items-center justify-between w-full gap-3 p-5 font-medium text-gray-500 border border-b-0 border-gray-200 rtl:text-right focus:ring-4 focus:ring-gray-200 hover:bg-gray-100 " data-accordion-target="#accordion-nested-collapse-body-2" aria-expanded="false" aria-controls="accordion-nested-collapse-body-2">
+                                        <span>Architecture</span>
+                                        <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5" />
+                                        </svg>
+                                    </button>
+                                </h2>
+                                <div id="accordion-nested-collapse-body-2" className="hidden" aria-labelledby="accordion-nested-collapse-heading-2">
+                                    <div className="p-5 border border-b-0 border-gray-200 ">
+                                        <p className="text-gray-500 ">Another difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers sections of pages.</p>
+                                    </div>
+                                </div>
+                                <h2 id="accordion-nested-collapse-heading-3">
+                                    <button type="button" className="flex items-center justify-between w-full gap-3 p-5 font-medium text-gray-500 border border-gray-200 rtl:text-right focus:ring-4 focus:ring-gray-200 hover:bg-gray-100 " data-accordion-target="#accordion-nested-collapse-body-3" aria-expanded="false" aria-controls="accordion-nested-collapse-body-3">
+                                        <span>Can I use both?</span>
+                                        <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5" />
+                                        </svg>
+                                    </button>
+                                </h2>
+                                <div id="accordion-nested-collapse-body-3" className="hidden" aria-labelledby="accordion-nested-collapse-heading-3">
+                                    <div className="p-5 border border-gray-200 ">
+                                        <p className="mb-2 text-gray-500 ">We actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best of two worlds.</p>
+                                        <p className="mb-2 text-gray-500 ">Learn more about these technologies:</p>
+                                        <ul className="text-gray-500 list-disc ps-5">
+                                            <li><a href="https://flowbite.com/pro/" className="text-blue-600 hover:underline">Flowbite Pro</a></li>
+                                            <li><a href="https://tailwindui.com/" rel="nofollow" className="text-blue-600 hover:underline">Tailwind UI</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* <!-- End: Nested accordion --> */}
+                        </div>
+                    </div>
+                    <h2 id="accordion-collapse-heading-2">
+                        <button type="button" className="flex items-center justify-between w-full gap-3 p-5 font-medium text-gray-500 border border-gray-200 rtl:text-right focus:ring-4 focus:ring-gray-200 hover:bg-gray-100" data-accordion-target="#accordion-collapse-body-2" aria-expanded="false" aria-controls="accordion-collapse-body-2">
+                            <span>Is there a Figma file available?</span>
+                            <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5" />
+                            </svg>
+                        </button>
+                    </h2>
+                    <div id="accordion-collapse-body-2" className="hidden" aria-labelledby="accordion-collapse-heading-2">
+                        <div className="p-5 border border-gray-200 ">
+                            <p className="mb-2 text-gray-500 ">Flowbite is first conceptualized and designed using the Figma software so everything you see in the library has a design equivalent in our Figma file.</p>
+                            <p className="text-gray-500 ">Check out the <a href="https://flowbite.com/figma/" className="text-blue-600 hover:underline">Figma design system</a> based on the utility classes from Tailwind CSS and components from Flowbite.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
