524d5c544d23f424a5a335a3c1dd5b244b964954
Jonas Geertsen Lund <jgl21@student.aau.dk>
2024-04-10 14:35:15 +0200

    remove console logs

--- TimeTrace/src/models/helpers/extractTimeStamp.ts
+++ TimeTrace/src/models/helpers/extractTimeStamp.ts
@@ -4,10 +4,7 @@ import { dateFormats } from "./dateFormats";
 export function extractTimeStamp(line: string): string {
     let timestamp: string | undefined;
 
-    // console.log(`LogFormatter.dateFormat: ${dateFormats[LogFormatter.dateFormat]}`)
-    // console.log(`Line: ` + line)
     let timestampMatch = line.match(dateFormats[LogFormatter.dateFormat]);
-    // console.log(`timestampMatch: ` + timestampMatch)
     timestamp = timestampMatch ? timestampMatch[0] : undefined;
     if (timestamp === undefined) {
         throw new Error(`Could not find a timestamp in log file line: '${line}'`);
