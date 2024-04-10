83c4fefb3cfb9b7a2f32278307c025de76337f00
Jonas Geertsen Lund <jgl21@student.aau.dk>
2024-04-10 08:28:58 +0200

    Indentation

--- TimeTrace/src/models/TREBuilder.ts
+++ TimeTrace/src/models/TREBuilder.ts
@@ -1,16 +1,16 @@
 import { TREParam } from "./TREParam";
 import { TREParser } from "./TREParser";
 export abstract class TREBuilder {
-  public static TREParams: TREParam[] = [];
+    public static TREParams: TREParam[] = [];
 
-  //TODO figure out what Anders does with regex mappings.
-  public static buildTRE(TREString: string, mappings: Map<string, string>): string {
-    TREParser.parseTRE(TREString, mappings);
+    //TODO figure out what Anders does with regex mappings.
+    public static buildTRE(TREString: string, mappings: Map<string, string>): string {
+        TREParser.parseTRE(TREString, mappings);
 
-    for (const TreParam of this.TREParams) {
-      TREString += TreParam.convertToTre();
+        for (const TreParam of this.TREParams) {
+            TREString += TreParam.convertToTre();
+        }
+
+        return TREString + "$";
     }
-
-    return TREString + "$";
-  }
 }
