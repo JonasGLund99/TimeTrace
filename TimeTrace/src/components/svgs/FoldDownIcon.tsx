b636e0856b02dbd1df6407ec52a9f9ae193aa879
caspermortensen <cmor21@student.aau.dk>
2024-04-15 10:14:53 +0200

    initial version of home, with todos that are due to later in project ( links, ref, spelling and video)

---
+++ TimeTrace/src/components/svgs/FoldDownIcon.tsx
@@ -0,0 +1,11 @@
+function FoldDownIcon() {
+
+    return (
+        <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m9 5 7 7-7 7" />
+        </svg>
+
+    )
+}
+
+export default FoldDownIcon;
