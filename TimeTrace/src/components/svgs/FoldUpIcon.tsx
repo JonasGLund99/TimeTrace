52b1e3a9ffc13f644bb0a39ec3dcecea47790271
Jonas Geertsen Lund <jgl21@student.aau.dk>
2024-04-16 08:24:23 +0200

    Update FoldUpIcon.tsx

--- TimeTrace/src/components/svgs/FoldUpIcon.tsx
+++ TimeTrace/src/components/svgs/FoldUpIcon.tsx
@@ -1,6 +1,6 @@
 function FoldUpIcon() {
     return (
-        <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+        <svg className="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7" />
         </svg>
     )
