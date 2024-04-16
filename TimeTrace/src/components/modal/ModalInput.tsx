d349e6279b89ea3a2e6f44ccab1dd1c464781ef2
unknown <Asbjoernjc@gmail.com>
2024-04-16 08:43:21 +0200

    ModalInput can be marked as required now

--- TimeTrace/src/components/modal/ModalInput.tsx
+++ TimeTrace/src/components/modal/ModalInput.tsx
@@ -4,14 +4,15 @@ interface IModalInputProps {
     type?: 'text' | 'number' | 'date';
     label: string;
     value: string;
+    required?: boolean;
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-function ModalInput({ placeholder, type, label, value, onChange }: IModalInputProps) {
+function ModalInput({ placeholder, type, label, value, onChange, required }: IModalInputProps) {
     return (
         <div className="flex flex-col w-full">
             <label>{label}</label>
-            <input value={value} onChange={onChange} className="px-2 py-2 border border-yellow-300 rounded-lg dark:border-yellow-800" placeholder={placeholder} type={type} ></input>
+            <input required={required} value={value} onChange={onChange} className="px-2 py-2 border border-yellow-300 rounded-lg dark:border-yellow-800" placeholder={placeholder} type={type} ></input>
         </div>
     )
 }
--- TimeTrace/src/components/predefined-tres/BetweenTRE.tsx
+++ TimeTrace/src/components/predefined-tres/BetweenTRE.tsx
@@ -18,12 +18,12 @@ function BetweenTRE({ treObject }: IBetweenTREProps) {
     return (
         <div className="flex flex-col gap-2">
             <div className="flex gap-4">
-                <ModalInput value={TREObject.input[0].value} onChange={(e) => updateModalValue(0, e.target.value)} label="First group of events" placeholder="Enter the first group of events" ></ModalInput>
-                <ModalInput value={TREObject.input[1].value} onChange={(e) => updateModalValue(1, e.target.value)} label="Second group of events" placeholder="Enter the second group of events" ></ModalInput>
+                <ModalInput required={true} value={TREObject.input[0].value} onChange={(e) => updateModalValue(0, e.target.value)} label="First group of events" placeholder="Enter the first group of events" ></ModalInput>
+                <ModalInput required={true} value={TREObject.input[1].value} onChange={(e) => updateModalValue(1, e.target.value)} label="Second group of events" placeholder="Enter the second group of events" ></ModalInput>
             </div>
             <div className="flex gap-4">
-                <ModalInput value={TREObject.input[2].value} onChange={(e) => updateModalValue(2, e.target.value)} label="Start time" placeholder="Enter the start time" ></ModalInput>
-                <ModalInput value={TREObject.input[3].value} onChange={(e) => updateModalValue(3, e.target.value)} label="End time" placeholder="Enter the end time" ></ModalInput>
+                <ModalInput required={true} value={TREObject.input[2].value} onChange={(e) => updateModalValue(2, e.target.value)} label="Start time" placeholder="Enter the start time" ></ModalInput>
+                <ModalInput required={true} value={TREObject.input[3].value} onChange={(e) => updateModalValue(3, e.target.value)} label="End time" placeholder="Enter the end time" ></ModalInput>
             </div>
         </div>
     )
