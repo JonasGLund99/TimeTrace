1ebf931707a2daa7dc0ef50abf90ce78ec930051
caspermortensen <cmor21@student.aau.dk>
2024-04-19 08:32:57 +0200

    remove redundant mapping in tests

--- TimeTrace/src/tests/models/TREBuilder.test.ts
+++ TimeTrace/src/tests/models/TREBuilder.test.ts
@@ -4,7 +4,7 @@ import { CustomMap } from "../../models/Types/EventMapping";
 describe('TREBuilder', () => {
     
     describe('convertTimeConstraint', () => {
-        test('when no timeunit it should be interpretet as ms', ()=>{
+        test('When no timeunit it should be interpretet as ms', ()=>{
             // Arrange
             const trimmedTRE = "(A|B)%(100,1000)";
             // Act
@@ -24,7 +24,7 @@ describe('TREBuilder', () => {
               expect(expectedConvertedTRE).toEqual(convertedTRE);
         });
 
-        test('should convert s to ms', ()=>{
+        test('Should convert s to ms', ()=>{
                 // Arrange
                 const trimmedTRE = "(A|B)%(100s,1000s)";
                 const expectedConvertedTRE = "(A|B)%(100000,1000000)";
@@ -35,7 +35,7 @@ describe('TREBuilder', () => {
             
         });
 
-        test('should convert m to ms', () => {
+        test('Should convert m to ms', () => {
                 // Arrange
                 const trimmedTRE = "(A|B)%(100m,1000m)";
                 const expectedConvertedTRE = "(A|B)%(6000000,60000000)";
@@ -45,7 +45,7 @@ describe('TREBuilder', () => {
                 expect(expectedConvertedTRE).toEqual(convertedTRE);
         });
 
-        test('should convert h to ms', ()=>{
+        test('Should convert h to ms', ()=>{
                 // Arrange
                 const trimmedTRE = "(A|B)%(100h,1000h)";
                 const expectedConvertedTRE = "(A|B)%(360000000,3600000000)";
@@ -56,7 +56,7 @@ describe('TREBuilder', () => {
             
         });
 
-        test('should convert d to ms', ()=>{
+        test('Should convert d to ms', ()=>{
                 // Arrange
                 const trimmedTRE = "(A|B)%(1d,10d)";
                 const expectedConvertedTRE = "(A|B)%(86400000,864000000)";
@@ -67,7 +67,7 @@ describe('TREBuilder', () => {
             
         });
 
-        test('should convert both s in first to ms and d in second to ms ', ()=>{
+        test('Should convert both s in first to ms and d in second to ms ', ()=>{
                 // Arrange
                 const trimmedTRE = "(A|B)%(100s,1d)";
                 const expectedConvertedTRE = "(A|B)%(100000,86400000)";
@@ -78,7 +78,7 @@ describe('TREBuilder', () => {
             
         });
 
-        test('should handle multiple constraint with different types', ()=>{
+        test('Should handle multiple constraint with different types', ()=>{
             // Arrange
             const trimmedTRE = "AB%(100s,1d)BC%(2h,1d)";
             const expectedConvertedTRE = "AB%(100000,86400000)BC%(7200000,86400000)";
@@ -89,7 +89,7 @@ describe('TREBuilder', () => {
     });
     });
     describe('buildTRE', () => {
-        test('correctly build tre from rawtre with unit and whitespace', ()=>{
+        test('Correctly build tre from rawtre with unit and whitespace', ()=>{
             // Arrange
             const rawTRE = "(A|   Z)    %(     100 s,      1000 h    )";
             const expectedFinalActualTRE = "((A|Z)%(100000,3600000000))$"
@@ -103,7 +103,7 @@ describe('TREBuilder', () => {
             expect(expectedFinalActualTRE).toEqual(finalActualTRE);
 
         });
-        test('should not return but throw Error from parser, (First number in time constraint must be smaller than the second number)', ()=>{
+        test('Should not return but throw Error from parser, (First number in time constraint must be smaller than the second number)', ()=>{
             // Arrange
             const rawTRE = "(A|   Z)    %(     1.1 h,  1 h    )";
             // const expectedFinalActualTRE = "((A|B)%(3960000,3600000))$"
@@ -121,7 +121,7 @@ describe('TREBuilder', () => {
     })
 
     describe('convertz', () => {
-        test('', ()=>{
+        test('Correctly convert z', ()=>{
             // Arrange
             const convertedTRE = "(AB)z(AB)%(100000,3600000000)"
             const expectedConvertedTre = "(AB)(A|B|Z)*(AB)%(100000,3600000000)"
