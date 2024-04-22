1d90d0a7560879b2c99d5597fddfaf00ffea3802
Jonas Geertsen Lund <jgl21@student.aau.dk>
2024-04-22 08:46:34 +0200

    Indentation

--- TimeTrace/src/components/logtable/searcher/ToggleShowLines.tsx
+++ TimeTrace/src/components/logtable/searcher/ToggleShowLines.tsx
@@ -14,8 +14,7 @@ function ShowLineToggle() {
             <select className="block h-8 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
                 onChange={(e) => {
                     handleShowFilterUpdate(e.target.value as ShowLinesMode);
-                }
-                }>
+                }}>
                 <option value={ShowLinesMode.ALL}>Show All Lines</option>
                 <option value={ShowLinesMode.MAPPED}>Show Mapped Lines</option>
                 <option value={ShowLinesMode.UNMAPPED}>Show Unmapped Lines</option>
