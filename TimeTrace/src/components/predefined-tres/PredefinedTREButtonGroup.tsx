216e35a2b6a4b22221227e7887421172ceb6e2cb
Jonas Geertsen Lund <jgl21@student.aau.dk>
2024-04-22 14:39:22 +0200

    transition

--- TimeTrace/src/components/predefined-tres/PredefinedTREButtonGroup.tsx
+++ TimeTrace/src/components/predefined-tres/PredefinedTREButtonGroup.tsx
@@ -8,7 +8,7 @@ interface PredefinedTREButtonGroupProps {
 function PredefinedTREButtonGroup({closeTRE}: PredefinedTREButtonGroupProps) {
     return (
         <div id="modal-button-container" className="flex justify-end pt-2">
-        <Button buttonStyle={ButtonStyle.None} type="button" onClick={closeTRE} style={{style: 'text-github-textWhiteBg bg-github-navbarBg border border-github-borderWhiteBg hover:bg-github-highlightedBtnNavBg focus:ring-4 focus:outline-none focus:ring-github-borderWhiteBg font-medium rounded-lg text-xs px-3 py-1.5 me-2 text-center inline-flex items-center'}}>
+        <Button buttonStyle={ButtonStyle.None} type="button" onClick={closeTRE} style={{style: 'text-github-textWhiteBg bg-github-navbarBg border border-github-borderWhiteBg hover:bg-github-highlightedBtnNavBg focus:ring-4 focus:outline-none focus:ring-github-borderWhiteBg font-medium rounded-lg text-xs px-3 py-1.5 me-2 text-center inline-flex items-center transition-all'}}>
             Cancel
         </Button>
         <Button type="submit" buttonStyle={ButtonStyle.Modal}>
