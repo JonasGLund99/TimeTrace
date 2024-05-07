4e7fce8b4bebe4dea93b8fdafb048de3a2e40e87
Asbjørn Christensen <69584571+AsbjoernJC@users.noreply.github.com>
2024-05-07 14:01:45 +0200

    Ascending

--- TimeTrace/src/models/helpers/validation.ts
+++ TimeTrace/src/models/helpers/validation.ts
@@ -46,7 +46,7 @@ function FileIsInAscendingOrder(lines: string[]): void {
             throw new Error(`Could not parse timestamp in line ${i + 1} or ${i + 2}.${lineContents}`);
         }
         
-        if (date1 >= date2) //if line1 >= than line2 it is not sorted
+        if (date1 > date2) //if line1 >= than line2 it is not sorted
             throw new Error(`File must be in strictly ascending order. Ordering failed at:${lineContents}`);
     }
 }
